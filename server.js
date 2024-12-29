43014637fb8ac65573d0e86d2b879f811d78f4a8
JungJun <dlvvkfl88@naver.com>
2024-12-30 00:56:04 +0900

    nodejs공부-1

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ app.listen(8080,() => {
     console.log('http://localhost:8080 에서 서버 실행중')
 })
 
+//html 파일을 본고 싶으면 send가 아닌 sendFile(__dirname + '/index.html')
+
 app.get('/',(요청,응답) => {
-    응답.send('반갑다')
+    응답.sendFile(__dirname + '/index.html')
 })
+
+app.get('/news',(요청,응답) => {
+    응답.send('오늘 비온다')
+})
+
+
+app.get('/shop',(요청,응답) => {
+    응답.send('쇼핑페이지 입니다')
+})
+
